b7f0746865b24cbfacf6187f2c8b312cfc11d0d1
Jose Cabarcas <jcabarcas@joonik.com>
2018-08-24 16:33:38 -0500

    test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-let Index = () => {
+const Index = () => {
   return <div>Hola react! Si o No ?</div>;
 };
-
 
 ReactDOM.render(<Index />, document.getElementById("root"));
